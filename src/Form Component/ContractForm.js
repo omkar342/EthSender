8e10e6f316fc43874e11ce411458a5c206207dc6
omkar342 <omkarjadhav095@gmail.com>
2023-08-04 17:24:35 +0530

    removed unwanted console.log

--- src/Form Component/ContractForm.js
+++ src/Form Component/ContractForm.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ContractForm.css";
 
 const ContractForm = (props) => {
-  console.log("value is", props.amountToBeSendToSmartContract);
   return (
     <div className="contract_form">
       <form
